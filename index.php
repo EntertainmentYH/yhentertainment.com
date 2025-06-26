5cb788d01d52e8e69f64f55a5df909a341ecde3b
Entertainment_YH <3329261270@qq.com>
2025-06-27 00:42:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1090,7 +1090,6 @@ if (!empty($config['site_start_date'])) {
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
     <script src="js/statistics-chart.js"></script>
-    <script src="js/statistics-chart.js"></script>
 
 </body>
 
